3df0a14a51bedeb513acbfb396c19a9aa224bdc9
traqy <btraquena@gmail.com>
2012-11-22 15:51:50 +0800

    Update multimechanize/utilities/newproject.py
    Added scripts setup_env.py and teardown_env.py in the multimech-newproject code generation. Also change the v_user.py script content.

--- multimechanize/utilities/newproject.py
+++ multimechanize/utilities/newproject.py
@@ -14,6 +14,8 @@ import sys
 CONFIG_NAME = 'config.cfg'
 SCRIPT_NAME = 'v_user.py'
 SCRIPTS_DIR = 'test_scripts'
+SETUP_ENV_SCRIPT_NAME = 'setup_env.py'
+TEARDOWN_ENV_SCRIPT_NAME = "teardown_env.py"
 
 
 CONFIG_CONTENT = """
@@ -24,7 +26,8 @@ results_ts_interval = 10
 progress_bar = on
 console_logging = off
 xml_report = off
-
+pre_run_script = setup_env.py
+post_run_script = teardown_env.py
 
 [user_group-1]
 threads = 3
@@ -38,26 +41,58 @@ script = %s
 
 
 SCRIPT_CONTENT = """
-import random
+import mechanize
 import time
-
-
-class Transaction(object):
+ 
+class Transaction:
     def __init__(self):
-        pass
-
-    def run(self):
-        r = random.uniform(1, 2)
-        time.sleep(r)
-        self.custom_timers['Example_Timer'] = r
-
-
+        self.custom_timers = {}
+    def run(self):        
+        start_timer = time.time()
+ 
+        self.story()
+ 
+        latency = time.time() - start_timer        
+        self.custom_timers['Example_Homepage'] = latency
+ 
+    def story(self):
+        br = mechanize.Browser()
+        br.set_handle_robots(False)
+        resp = br.open('http://www.example.com/')
+        resp.read()        
+ 
 if __name__ == '__main__':
     trans = Transaction()
     trans.run()
     print trans.custom_timers
 """
 
+SETUP_ENV_SCRIPT_CONTENT = """
+ 
+class Transaction(object):
+ 
+    def run(self):
+        # Initializations of your test project environment should be done such as the following:
+        #  * Initialize data on database engine such as MySQL if your test cases require presetup data on database backend
+        #  * Generate and populate huge data to a filesystem storage
+        foo="setup your environment logic here"
+ 
+if __name__ == '__main__':
+    trans = Transaction()
+    trans.run()
+"""
+
+TEARDOWN_ENV_SCRIPT_CONTENT = """
+class Transaction(object):
+ 
+    def run(self):
+        # Teardown logic must be specified here such deleting data created by the setup_env.py or created during the multimech-run project.
+        bar="Define your story logic here on how to destroy your environment."
+ 
+if __name__ == '__main__':
+    trans = Transaction()
+    trans.run()
+"""
 
 def create_project(
         project_name,
@@ -66,6 +101,12 @@ def create_project(
         scripts_dir=SCRIPTS_DIR,
         config_content=CONFIG_CONTENT,
         script_content=SCRIPT_CONTENT,
+
+        script_setup_env_name=SETUP_ENV_SCRIPT_NAME,
+        script_setup_env_content=SETUP_ENV_SCRIPT_CONTENT,
+        script_teardown_env_name=TEARDOWN_ENV_SCRIPT_NAME,
+        script_teardown_env_content=TEARDOWN_ENV_SCRIPT_CONTENT
+
     ):
     if os.path.exists(project_name):
         sys.stderr.write('\nERROR: project already exists: %s\n\n' % project_name)
@@ -80,6 +121,10 @@ def create_project(
         f.write(config_content)
     with open(os.path.join(project_name, scripts_dir, script_name), 'w') as f:
         f.write(script_content)
+    with open(os.path.join(project_name, scripts_dir, script_setup_env_name), 'w') as f:
+        f.write(script_setup_env_content)
+    with open(os.path.join(project_name, scripts_dir, script_teardown_env_name), 'w') as f:
+        f.write(script_teardown_env_content)
 
 
 def main():
